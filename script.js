04951c2e4a30e9a79f61c436df98162b8dd4f4b7
Matheus Benedett <118411985+matheusbenedett@users.noreply.github.com>
2025-05-12 18:38:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const orderBumps = document.querySelectorAll('.bump-details')
-
-orderBumps.forEach(item => {
-  item.addEventListener('click', () => {
-    const input = item.querySelector('input');
-    if (input) {
-      input.click();
-    }
+document.addEventListener("DOMContentLoaded", function(event){
+  const orderBumps = document.querySelectorAll('.bump-details')
+  
+  orderBumps.forEach(item => {
+    item.addEventListener('click', () => {
+      const input = item.querySelector('input');
+      if (input) {
+        input.click();
+      }
+    });
   });
 });
+
+
